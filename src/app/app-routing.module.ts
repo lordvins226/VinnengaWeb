70ae92c40ae7daa126599d9c9e57202e2502e497
lordvins226 <kevineilboudo54@gmail.com>
2020-08-07 18:17:10 +0000

    :hatching_chick: initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SiteLayoutComponent } from './site-layout/site-layout.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+    path: '',
+    component: AdminLayoutComponent,
+    children : [
+      { path: 'Login', component: LoginComponent,pathMatch: 'full'},
+      {path: 'Dashboard', component: DashboardComponent},
+      {path:'', component: DashboardComponent},
+    ]
+},
+{
+  path: '',
+  component: SiteLayoutComponent,
+  children : [
+      { path: 'Home', component: HomeComponent },
+      { path: 'About', component: AboutComponent },      
+  ]
+},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SiteLayoutComponent } from './site-layout/site-layout.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteLayoutComponent,
+    AdminLayoutComponent,
+    LoginComponent,
+    DashboardComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
